61088ec4a890bfe3737aa61b2e5ace639073949e
Emre Ergül <emredevsalot@gmail.com>
2023-09-13 20:21:53 +0300

    fix events' execute functions' argument types

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,9 +1,10 @@
 const { Events } = require("discord.js");
+import type { CacheType, Interaction } from "discord.js";
 
 // Command interactions listener
 module.exports = {
   name: Events.InteractionCreate,
-  async execute(interaction: any) {
+  async execute(interaction: Interaction<CacheType>) {
     if (!interaction.isChatInputCommand()) return;
 
     const command = interaction.client.commands.get(interaction.commandName);
@@ -23,41 +24,3 @@ module.exports = {
     }
   },
 };
-
-// TODO:
-// The part below used to work in bot.ts
-// Can't use "Interaction<CacheType>" here.
-// ERROR: 'Interaction' refers to a value, but is being used as a type here. Did you mean 'typeof Interaction'?ts(2749)
-
-// // Receiving command interactions
-// client.on(
-//   Events.InteractionCreate,
-//   async (interaction: Interaction<CacheType>) => {
-//     if (!interaction.isChatInputCommand()) return;
-//     const command = client.commands.get(interaction.commandName);
-
-//     if (!command) {
-//       console.error(
-//         `No command matching ${interaction.commandName} was found.`
-//       );
-//       return;
-//     }
-
-//     try {
-//       await command.execute(interaction);
-//     } catch (error) {
-//       console.error(error);
-//       if (interaction.replied || interaction.deferred) {
-//         await interaction.followUp({
-//           content: "There was an error while executing this command!",
-//           ephemeral: true,
-//         });
-//       } else {
-//         await interaction.reply({
-//           content: "There was an error while executing this command!",
-//           ephemeral: true,
-//         });
-//       }
-//     }
-//   }
-// );
--- events/ready.ts
+++ events/ready.ts
@@ -1,20 +1,11 @@
 const { Events } = require("discord.js");
+import type { Client } from "discord.js";
 
 // When the client is ready, run this code (only once)
 module.exports = {
   name: Events.ClientReady,
   once: true,
-  execute(client: any) {
+  execute(client: Client<true>) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
   },
 };
-
-// TODO:
-// The part below used to work in bot.ts
-// Can't use "Client<true>" here.
-
-// // When the client is ready, run this code (only once)
-// // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-// client.once(Events.ClientReady, (c: Client<true>) => {
-//   console.log(`Ready! Logged in as ${c.user.tag}`);
-// });
