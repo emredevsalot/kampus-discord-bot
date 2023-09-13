2b0c383aa0b6653fce847a58ec08fb55e3148818
Emre Ergül <emredevsalot@gmail.com>
2023-09-13 12:52:56 +0300

    get interactions with bot

--- bot.ts
+++ bot.ts
@@ -1,14 +1,81 @@
-// import discord.js
-import { Client, Events, GatewayIntentBits } from "discord.js";
+const fs = require("node:fs");
+const path = require("node:path");
+// Require the necessary discord.js classes
+import {
+  CacheType,
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  Interaction,
+} from "discord.js";
 
-// create a new Client instance
+// Create an instance of Client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+// Dynamically retrieve command files
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file: any) => file.endsWith(".ts"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
+client.once(Events.ClientReady, (c: Client<true>) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
+
+// Receiving command interactions
+client.on(
+  Events.InteractionCreate,
+  async (interaction: Interaction<CacheType>) => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) {
+      console.error(
+        `No command matching ${interaction.commandName} was found.`
+      );
+      return;
+    }
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        });
+      }
+    }
+  }
+);
 
 // login with the token from .env.local
 client.login(process.env.DISCORD_TOKEN);
