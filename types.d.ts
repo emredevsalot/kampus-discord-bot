2b3682b512f85bc5741195377602750c4752f5e2
Emre Ergül <emredevsalot@gmail.com>
2023-09-13 12:51:44 +0300

    attach commands to the Client

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+import { Client, Collection } from "discord.js";
+
+declare module "discord.js" {
+  interface Client {
+    // We attached a "commands" property to the client instance
+    // so that we can access commands in other files.
+    commands: Collection<string, any>;
+  }
+}
